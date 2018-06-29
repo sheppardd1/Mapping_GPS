4f92be4810aa39c2af5c8486a4ef22f57cfa33e8
sheppardd1 <sheppardd1@users.noreply.github.com>
2018-06-29 13:48:25 -0500

    converted arrays to Linked Lists

--- app/src/main/java/com/example/davea/mapping_gps/MapsActivity.java
+++ app/src/main/java/com/example/davea/mapping_gps/MapsActivity.java
@@ -33,6 +33,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.LinkedList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -40,8 +41,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public GoogleMap gMap;
 
     //Constants:
-    //final int UPDATE_INTERVAL = 3000;   //when on, update location data every UPDATE_INTERVAL milliseconds
-    final int ARRAY_SIZE_MAX = 10000;   //max size of dataArray and timeArray. Process is stopped and warning shown is array size is exceeded in locationDetails()
     static final String filename = "GPS_data.txt";  //name of file where data is saved
 
     //Location:
@@ -61,12 +60,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     double currentLatitude;
     LatLng currentPosition;
     public int numPins = 0; //number of markers on map
-    Float dataArray[] = new Float[ARRAY_SIZE_MAX];  //array of the accuracy readings
+    LinkedList<Float> dataList = new LinkedList<Float>();
     float average = -999;   //average of the accuracy readings from one session. Initialize to -999 so any errors are obvious
     static String fileContents; //Stuff that will be written to the file. It is static so that it can be accessed in other activity
     String markerLabel = "X";  //label for the marker (accuracy and number of marker example: 3.0 #2)
     String time;    //the time in the dateFormatDayAndTime format (defined later). Used for giving start and end times of each session
-    String timeArray[] = new String[ARRAY_SIZE_MAX];    //array of the times in the dateFormatTime format (defined later). Used for time label of each data point
+    LinkedList<String> timeList = new LinkedList<String>(); //List of all the times that the datapoints were taken
     boolean wasReset = true;    //true if session data has been reset
     boolean setStartTime = false;   //true if start time of session has been set. Ensures that start time is only set at the beginning of a session
     public boolean on = false;  //true if session is running, not paused or stopped
@@ -82,8 +81,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     SimpleDateFormat dateFormatTime;
     SimpleDateFormat dateFormatDayAndTime;
 
-
-//TODO: Turn Arrays into Linked Lists
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -126,7 +123,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 } else if (numPins != 0) {      //if data has been collected but was not on
                     long sum = (long) 0.0;  //initialize sum to 0
                     for (int i = 0; i < numPins; i++) { //get sum of all accuracy reading during the session
-                        sum += dataArray[i];
+                        sum += dataList.get(i);
                     }
                     average = sum / ((float) numPins); //compute average
                     TV.setText(""); //clear TextView
@@ -157,10 +154,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         dateFormatTime = new SimpleDateFormat("hh:mm:ss aa");
         dateFormatDayAndTime = new SimpleDateFormat("MMM dd, yyyy hh:mm aa");
 
-        for (int i = 0; i < ARRAY_SIZE_MAX; i++) {  //initialize both arrays to null
-            dataArray[i] = null;
-            timeArray[i] = null;
-        }
+        dataList.clear();
+        timeList.clear();
+
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE); //set up location manager
 
         //define buttons and textview
@@ -178,16 +174,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         FileOutputStream outputStream;  //declare output stream
         try {   //attempt to open and write to file
             outputStream = openFileOutput(filename, Context.MODE_PRIVATE);  //open file and set to output stream
-            for (int i = 0; dataArray[i] != null && i < ARRAY_SIZE_MAX; i++) {
+            for (int i = 0; i < numPins; i++) {
                 writeData(i, outputStream); //write data
-                //set array values at current index to null now that their contents have been written to the file (resetting them)
-                dataArray[i] = null;
-                timeArray[i] = null;
             }
+            dataList.clear();
+            timeList.clear();
+
             outputStream.close(); //close file
         } catch (Exception e) { //if file is not found, catch exception
             e.printStackTrace();
-            TV.setText("ERROR: FILE NOT FOUND\nDATA NOT WRITTEN");  //error message if file not found
+            TV.setText("ERROR: \nDATA NOT WRITTEN");  //error message if file not found
         }
 
         //reset values
@@ -213,8 +209,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         //set fileContents to number, accuracy value, and timestamp [example: "#1)  9.0"  ] with fancy formatting
-        fileContents += ("#" + (i + 1) + ") \t\t" +(dataArray[i] + " \t\t" + timeArray[i] + "\n"));
-        if (dataArray[i + 1] == null) {  //end of data that must be written is reached
+        fileContents += ("#" + (i + 1) + ") \t\t" +(dataList.get(i).toString() + " \t\t" + timeList.get(i) + "\n"));
+        if (i == numPins - 1) {  //end of data that must be written is reached
             fileContents += "\nAverage: " + average + "\n\n"; //write the average and add some endlines
             try {   //write file
                 outputStream.write(fileContents.getBytes());    //write fileContents into file
@@ -314,41 +310,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         currentLatitude = location.getLatitude();
                         //set lat and long into LatLng type variable
                         currentPosition = new LatLng(currentLatitude, currentLongitude);
-                        if(numPins < ARRAY_SIZE_MAX) {  //if arrays are not full, write to them
-                            //put accuracy value into array
-                            dataArray[numPins] = location.getAccuracy();
-                            TV.setText("Running - #" + (numPins + 1) + " - " + dataArray[numPins]);
-                            //get time stamp
-                            timeArray[numPins] = dateFormatTime.format(location.getTime());
-                            //set label for marker (accuracy and marker number)
-                            markerLabel = (dataArray[numPins] + " #" + (++numPins));
-                            //create instance of MarkerOptions
-                            MarkerOptions markerOptions = new MarkerOptions();
-                            //set marker options:
-                            markerOptions.position(currentPosition);    //location of marker
-                            markerOptions.title(markerLabel);   //label for marker
-                            if(dataArray[numPins - 1] < 10) {   //if small error margin, marker is green
-                                //note: numPins was previously incremented, so use numPins-1 as index
-                                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-                            }
-                            else if (dataArray[numPins - 1] < 25)   //if 10 <= accuracy < 25, yellow marker
-                            {
-                                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
-                            }
-                            else if (dataArray[numPins - 1] < 100){   //else red marker for 25 <= accuracy < 100
-                                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-                            }
-                            else { //else if accuracy >= 100
-                                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
-                            }
-                            //add marker
-                            gMap.addMarker(markerOptions);
-                            //update camera position
-                            gMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
+                        dataList.add(location.getAccuracy());
+                        TV.setText("Running - #" + (numPins + 1) + " - " + location.getAccuracy());
+                        //get time stamp
+                        timeList.add(dateFormatTime.format(location.getTime()));
+                        //set label for marker (accuracy and marker number)
+                        markerLabel = dataList.get(numPins) + " #" + (++numPins);
+                        //create instance of MarkerOptions
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        //set marker options:
+                        markerOptions.position(currentPosition);    //location of marker
+                        markerOptions.title(markerLabel);   //label for marker
+                        if(dataList.get(numPins - 1) < 10) {   //if small error margin, marker is green
+                            //note: numPins was previously incremented, so use numPins-1 as index
+                            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
                         }
-                        else{   //if arrays are full, say so
-                            TV.setText("ERROR: ARRAY FULL\nFURTHER DATA CANNOT BE WRITTEN\nPROCESS STOPPED");
+                        else if (dataList.get(numPins - 1) < 25)   //if 10 <= accuracy < 25, yellow marker
+                        {
+                            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
                         }
+                        else if (dataList.get(numPins - 1) < 100){   //else red marker for 25 <= accuracy < 100
+                            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                        }
+                        else { //else if accuracy >= 100
+                            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
+                        }
+                        //add marker
+                        gMap.addMarker(markerOptions);
+                        //update camera position
+                        gMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
 
                     }
                 }
