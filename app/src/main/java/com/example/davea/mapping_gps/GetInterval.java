067893380d90d79ec46e3a6b9d0984454e0c87ec
sheppardd1 <sheppardd1@users.noreply.github.com>
2018-07-12 17:09:47 -0500

    minor .txt file formatting

--- app/src/main/java/com/example/davea/mapping_gps/GetInterval.java
+++ app/src/main/java/com/example/davea/mapping_gps/GetInterval.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GetInterval extends AppCompatActivity {
 
@@ -25,9 +26,11 @@ public class GetInterval extends AppCompatActivity {
         //print instrucitons
         TV.setText(R.string.GetInterval_Instructions);
 
+
         done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                boolean validValues = true; //only false if lat and lng values are both inputted and at least one is outside of acceptable range, else true
                 //note: using an unsigned EditText for interval, so don't have to worry about negative numbers there
                 if(interval_input.getText().toString().trim().length() > 0) {   //ensure interval_input is not empty
                     MapsActivity.interval = Integer.valueOf(interval_input.getText().toString()); //set interval to value specified in interval_input
@@ -37,18 +40,37 @@ public class GetInterval extends AppCompatActivity {
                         MapsActivity.locationManager.removeUpdates(MapsActivity.locationListener);  //ensures we only have one location listener running at once. Don't want duplicate data.
                     }
                     //if user has entered both a true lat and long value, set them
-                    //if entered only one, ignore it
+                    //if entered only one or none, ignore it
                     if(lat_input.getText().toString().trim().length() > 0 && lng_input.getText().toString().trim().length() > 0){
-                        MapsActivity.setTrueLatLng = true;
-                        //set values in MapsActivity
-                        MapsActivity.trueLat = Float.valueOf(lat_input.getText().toString());
-                        MapsActivity.trueLng = Float.valueOf(lng_input.getText().toString());
+                        if(Math.abs(Float.valueOf(lat_input.getText().toString())) > 90)
+                        {   //lat values must be between -90 and 90 inclusive
+                            Toast.makeText(GetInterval.this, "Invalid Latitude Value", Toast.LENGTH_SHORT).show();
+                            validValues = false;
+                        }
+                        else if(Math.abs(Float.valueOf(lng_input.getText().toString())) > 180)
+                        {   //lng vlaues must be between -180 and 180 inclusive
+                            Toast.makeText(GetInterval.this, "Invalid Longitude Value", Toast.LENGTH_SHORT).show();
+                            validValues = false;
+                        }
+                        else {
+                            validValues = true;
+                            MapsActivity.setTrueLatLng = true;
+                            //set values in MapsActivity
+                            MapsActivity.trueLat = Float.valueOf(lat_input.getText().toString());
+                            MapsActivity.trueLng = Float.valueOf(lng_input.getText().toString());
+                        }
                     }
                     else {  //else did not specify both true lat and long
                         MapsActivity.setTrueLatLng = false;
+                        validValues = true;
                     }
-                    //go back to main activity
-                    startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+                    if(validValues) {
+                        //go back to main activity
+                        startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+                    }
+                }
+                else{
+                    Toast.makeText(GetInterval.this, "Must Enter Interval Value", Toast.LENGTH_SHORT).show();
                 }
             }
         });
--- app/src/main/java/com/example/davea/mapping_gps/MapsActivity.java
+++ app/src/main/java/com/example/davea/mapping_gps/MapsActivity.java
@@ -243,15 +243,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             //print accuracy value on screen along with coordinates and time
             time = dateFormatDayAndTime.format(cal.getTime());
             fileContents += " Stop:  " + time + "\n------------------------------\n";
-            if(setTrueLatLng) fileContents += "# | Accuracy | Error (m) | Time\n";
-            else fileContents += "# | Accuracy | Time\n";
+            if(setTrueLatLng) fileContents += "#  | Accuracy |  Error (m)  | Time\n";
+            else fileContents += "#  | Accuracy | Time\n";
         }
 
         //set fileContents to number, accuracy value, and timestamp [example: "#1)  9.0"  ] with fancy formatting
         //fileContents += ("#" + (i + 1) + ") \t\t" +(dataList.get(i).toString() + " \t\t" + (String.format("%.2f", distanceErrorList.get(i))) + " \t\t" + timeList.get(i) + "\n"));
         if(setTrueLatLng) {
-            fileContents += String.format("%-5s %s", ((i + 1) + ")"), String.format("%-10s %s", (dataList.get(i).toString()), String.format("%-9s %s", (String.format("%.2f", distanceErrorList.get(i))), (timeList.get(i) + "\n"))));
-            //note: distanceErrorList.get(i) will never be more than 8 digits since the earth's circumference is about 40 million meters, so formatting will never truncate digits
+            fileContents += String.format("%-5s %s", ((i + 1) + ")"), String.format("%-10s %s", (dataList.get(i).toString()), String.format("%-11s %s", (String.format("%.2f", distanceErrorList.get(i))), (timeList.get(i) + "\n"))));
+            //note: distanceErrorList.get(i) will never be more than 11 chars since the earth's circumference is about 40 million meters (XXXXXXXX.XX), so formatting will never truncate digits
         }
         else{
             fileContents += String.format("%-7s %s", ("#" + (i + 1) + ")"), String.format("%-10s %s", (dataList.get(i).toString()), (timeList.get(i) + "\n")));
@@ -259,7 +259,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         if (i == numPins - 1) {  //end of data that must be written is reached
-            fileContents += "\nAverage: " + average + "\n\n"; //write the average and add some endlines
+            fileContents += "\nAverage Accuracy: " + average + "\n\n"; //write the average and add some endlines
             try {   //write file
                 outputStream.write(fileContents.getBytes());    //write fileContents into file
                 TV.setText(TV.getText() + "File Written");
